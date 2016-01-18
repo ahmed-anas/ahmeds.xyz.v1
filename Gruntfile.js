75986c2da80cbbcf0dbb33e5b3814d6be6b6c16c
Ahmed Anas <syed.ahmed.anas@gmail.com>
2016-01-18 17:13:24 +0500

    added a few logos

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var fs = require('fs');
+
+module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-wiredep');
+
+    grunt.initConfig({
+        wiredep: {
+            app: {
+                src: 'public/index.html'
+            }
+        }
+    });
+    
+}
+
